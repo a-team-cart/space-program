dd3f647bf682e4b58104ff789bcc31ba90c2be4c
Kodrin <kodrintxps@gmail.com>
2019-03-04 12:04:27 -0500

    added junk terminal script

---
+++ Assets/Scripts/Terminals/JunkTerminal.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JunkTerminal : MonoBehaviour
+{
+	[Header("Spawner")]
+
+	public GameObject _spawnLocation; 
+	public GameObject _junkObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //init function at the start
+        init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //the behaviour for the junk script 
+        JunkBehaviour();
+    }
+
+    public void init(){
+	
+    }
+
+    public void JunkBehaviour(){
+
+
+    	//INTERACTION TO SPAWN STUFF, PREFERABLY A BUTTON
+    	if(Input.GetKeyDown("space"))
+    		Instantiate(_junkObject, _spawnLocation.transform.position, _spawnLocation.transform.rotation);
+    }
+}
