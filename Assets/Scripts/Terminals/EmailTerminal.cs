2b8bb8ecae6261fc24755671299d0996cdb7fcfd
Codrin Tablan Negrei <kodrintxps@gmail.com>
2019-03-25 13:10:35 -0400

    Added JSON database/Imported JSON utilities/Added EmailTerminal Script

---
+++ Assets/Scripts/Terminals/EmailTerminal.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using SimpleJSON;
+
+public class EmailTerminal : MonoBehaviour
+{
+	[Header("Email List Parameters")]
+	public string m_emailDataFileName = "emails.json";
+
+	public List<string>	m_sendersList = new List<string>();
+	public List<string>	m_messagesList = new List<string>();
+
+
+	[System.Serializable]
+	public class Email
+	{
+	    public string m_sender;
+	    public string m_message;
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		//load all the emails
+		LoadEmails();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //loads all the emails from the streaming file directory
+    private void LoadEmails()
+    {
+    	// Path.Combine combines strings into a file path
+        // Application.StreamingAssets points to Assets/StreamingAssets in the Editor, and the StreamingAssets folder in a build
+        string filePath = Path.Combine(Application.streamingAssetsPath, m_emailDataFileName);
+
+        //if the path exists
+        if(File.Exists(filePath))
+        {
+            // Read the json from the file into a string
+            string dataAsJson = File.ReadAllText(filePath); 
+            // Pass the json to JsonUtility, and tell it to create a GameData object from it
+            // Email[] emails = JsonHelper.getJsonArray<Email>(dataAsJson);
+            var N = JSON.Parse(dataAsJson);
+			string emails = N["emails"][0].Value;
+			Debug.Log(emails);
+            // Retrieve the allRoundData property of loadedData
+            // allRoundData = loadedData.allRoundData;
+
+            //DEBUG
+            // Debug.Log(emails[0]);
+        }
+        else
+        {
+            Debug.LogError("Cannot load game data!");
+        }
+    }
+}
---
+++ Assets/Scripts/Terminals/JsonHelper.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class JsonHelper
+{
+    public static T[] getJsonArray<T>(string json)
+    {
+        string newJson = "{ \"array\": " + json + "}";
+        WrapperA<T> wrapper = JsonUtility.FromJson<WrapperA<T>> (newJson);
+        return wrapper.array;
+    }
+
+    [System.Serializable]
+    private class WrapperA<T>
+    {
+        public T[] array;
+    }
+
+    public static T[] FromJson<T>(string jsonArray)
+    {
+        jsonArray = WrapArray (jsonArray);
+        return FromJsonWrapped<T> (jsonArray);
+    }
+
+    public static T[] FromJsonWrapped<T> (string jsonObject)
+    {
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(jsonObject);
+        return wrapper.items;
+    }
+
+    private static string WrapArray (string jsonArray)
+    {
+        return "{ \"items\": " + jsonArray + "}";
+    }
+
+    public static string ToJson<T>(T[] array)
+    {
+        Wrapper<T> wrapper = new Wrapper<T>();
+        wrapper.items = array;
+        return JsonUtility.ToJson(wrapper);
+    }
+
+    public static string ToJson<T>(T[] array, bool prettyPrint)
+    {
+        Wrapper<T> wrapper = new Wrapper<T>();
+        wrapper.items = array;
+        return JsonUtility.ToJson(wrapper, prettyPrint);
+    }
+
+    [System.Serializable]
+    private class Wrapper<T>
+    {
+        public T[] items;
+    }
+}
