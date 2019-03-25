ac4a32b79c570498de23315016c4c917093dfa55
Codrin Tablan Negrei <kodrintxps@gmail.com>
2019-03-25 14:28:59 -0400

    Email/database now complete, you can access them

--- Assets/Scripts/Terminals/EmailTerminal.cs
+++ Assets/Scripts/Terminals/EmailTerminal.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 using System.IO;
 using SimpleJSON;
 
+/*
+References on JSON serialization/deserialization : 
+- https://stackoverflow.com/questions/36239705/serialize-and-deserialize-json-and-json-array-in-unity
+- https://docs.unity3d.com/ScriptReference/JsonUtility.FromJson.html
+*/
+
 public class EmailTerminal : MonoBehaviour
 {
+	[Header("Am I debugging?")]
+	public bool m_isDebugging = false;
+
 	[Header("Email List Parameters")]
 	public string m_emailDataFileName = "emails.json";
 
-	public List<string>	m_sendersList = new List<string>();
-	public List<string>	m_messagesList = new List<string>();
+	public int m_numberOfEmails = 75; 
+
+	// public List<string>	m_sendersList = new List<string>();
+	// public List<string>	m_messagesList = new List<string>();
+
+	public List<Email>	m_emailList = new List<Email>();
 
 
 	[System.Serializable]
@@ -18,6 +31,12 @@ public class EmailTerminal : MonoBehaviour
 	{
 	    public string m_sender;
 	    public string m_message;
+
+	    public Email (string sender, string message)
+	    {
+	    	m_sender = sender;
+	    	m_message = message;
+	    }
 	}
 
     // Start is called before the first frame update
@@ -45,16 +64,21 @@ public class EmailTerminal : MonoBehaviour
         {
             // Read the json from the file into a string
             string dataAsJson = File.ReadAllText(filePath); 
-            // Pass the json to JsonUtility, and tell it to create a GameData object from it
-            // Email[] emails = JsonHelper.getJsonArray<Email>(dataAsJson);
-            var N = JSON.Parse(dataAsJson);
-			string emails = N["emails"][0].Value;
-			Debug.Log(emails);
-            // Retrieve the allRoundData property of loadedData
-            // allRoundData = loadedData.allRoundData;
 
-            //DEBUG
-            // Debug.Log(emails[0]);
+            // Pass the json to JsonUtility, and tell it to create a GameData object from it
+            var N = JSON.Parse(dataAsJson);
+
+            for(int i = 0; i < m_numberOfEmails; i ++)
+            {
+            	// m_sendersList.Add(N["emails"][i]["sender"].Value);
+            	// m_messagesList.Add(N["emails"][i]["message"].Value);
+
+				m_emailList.Add(new Email(N["emails"][i]["sender"].Value ,N["emails"][i]["message"].Value));
+
+
+            	if(m_isDebugging)
+            		Debug.Log(N["emails"][i]["sender"].Value + N["emails"][i]["message"].Value);
+            }
         }
         else
         {
